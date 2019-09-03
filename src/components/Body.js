8e5d907a5470b48b7d912a00e6082c46f3a7d9b6
sam3106 <sampson2448@yahoo.com>
2019-09-02 21:29:07 -0400

    third commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,30 +4,36 @@ import './Body.css';
 class Body extends React.Component {
   constructor() {
     super();
+    const Style = {
+      fontSize: '84px',
+      textAlign: 'center'
+    };
   }
+
   render() {
     return (
       <div id="body">
         <div id="body-container">
-          <div className="profile-info">
-            <img
-              id="profile-pic"
-              src="https://www.absoluteanime.com/naruto/sasuke.jpg"
-            />
-            <div className="profile-user-info">
-              <h2>Sasuke Uchiha</h2>
-              <button id="edit-profile-button">Edit Profile</button>
-              <a>
-                <i class="big cog icon"></i>
-              </a>
-            </div>
-            <div>
-              <ul>
-                <li>11 post</li>
-                <li> 19 followers</li>
-                <li>3 following</li>
-              </ul>
-            </div>
+          <div id="profile-image">
+            <img src="https://www.absoluteanime.com/naruto/sasuke.jpg" />
+          </div>
+          <div className="profile-user-info">
+            <h2>Sasuke Uchiha</h2>
+            <button id="edit-profile-button">Edit Profile</button>
+            <a>
+              <i class="big cog icon"></i>
+            </a>
+            <ul>
+              <li>
+                <span>11 </span> post
+              </li>
+              <li>
+                <span>19 </span> followers
+              </li>
+              <li id="lastLi">
+                <span>3 </span> following
+              </li>
+            </ul>
           </div>
         </div>
       </div>
